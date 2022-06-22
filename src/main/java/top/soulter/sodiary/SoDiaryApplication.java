ddef34e4d7709afcac99b34a0f585237498939a5
Soulter <37870767+Soulter@users.noreply.github.com>
2022-06-23 00:44:37 +0800

    pref: 完成了页面大部分设计（也许）。

---
+++ src/main/java/top/soulter/sodiary/SoDiaryApplication.java
@@ -0,0 +1,13 @@
+package top.soulter.sodiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/soulter/sodiary/SoDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package top.soulter.sodiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
